c16c7c440ba1c4386563111c01fc719ecd208597
XWalways <718309617xw@gmail.com>
2021-03-15 23:12:36 -0400

    del

--- resnest50_market_triplet_labelsmooth.sh
+++
@@ -1,3 +0,0 @@
-python main.py --mode train --backbone resnest50 --data_path ./Market-1501-v15.09.15 --labelsmooth --triplet --stage 1 --save_path weights_resnest50_market_triplet_labelsmooth
-#python main.py --mode train --backbone resnest50 --data_path ./Market-1501-v15.09.15 --stage 2 --save_path weights_resnest50_market_triplet_labelsmooth
-#python main.py --mode train --backbone resnest50 --data_path ./Market-1501-v15.09.15 --stage 3 --save_path weights_resnest50_market_triplet_labelsmooth
